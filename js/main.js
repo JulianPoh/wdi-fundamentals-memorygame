293574cbf569d5a0aaa17485aa39d6ed5faf8747
Julian Poh <julianpoh01@gmail.com>
2018-05-16 15:45:51 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 console.log("Up and running!");
 
-var cards = ["queen","queen","king","king"];
+var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
+checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again.");
+		}
+	}
+}
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+flipCard = function(cardId) {
+	
+	console.log("User flipped "+cards[cardId]);
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}	else alert("Sorry, try again.");
+	cardsInPlay.push(cards[cardId]);
+
+
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
